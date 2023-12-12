7ecbc5a445f47def3ede142b5534799531ac1865
Pedro de Abreu Palma <pedroabreupalma@gmail.com>
2023-12-12 19:48:01 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -4,9 +4,10 @@ idades = 0
 nome_mais_velho = ''
 
 for c in range(1, 5):
-    nome = input('Nome da {} pessoa: '.format(c))
-    idade = int(input('Idade da {} pessoa: '.format(c)))
-    sexo = input('Sexo da {} pessoa (masculino/feminino): '.format(c)).lower()
+    print('---- {}° Pessoa ----'.format(c))
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    sexo = input('Sexo(masculino/feminino): ').lower()
 
     idades += idade
     mediat = idades / c
@@ -22,5 +23,5 @@ for c in range(1, 5):
         menos += 1
 
 print('A média de idade do grupo é {:.2f}'.format(mediat))  # Ajustei para mostrar a média com duas casas decimais
-print('O nome do mais velho do grupo é {}'.format(nome_mais_velho).upper())
+print('O nome do mais velho do grupo é {} e ele tem {} anos'.format(nome_mais_velho, idade))
 print('No total, abaixo de 20 anos, há {} mulher(es)'.format(menos))
